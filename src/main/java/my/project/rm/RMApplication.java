80ae4ecff0a399ab26ba388a04ca02cddf93a8d3
DDemoNZ <zinchukdimaa@gmail.com>
2021-03-05 10:14:03 +0200

    Initial commit

---
+++ src/main/java/my/project/rm/RMApplication.java
@@ -0,0 +1,13 @@
+package my.project.rm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/project/rm/RMApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.rm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
